85dfae2b53f3b23d204f41d4c48c496bfdb818e2
jamaludeen <justjamal17@gmail.com>
2022-09-29 15:30:09 +0530

    function

---
+++ index.js
@@ -0,0 +1,16 @@
+function msg(){  
+alert("hello! U clicked the alert");  
+}  
+
+//functionwith arguments
+
+function getcube(number){  
+alert(number*number*number);  
+}  
+
+//Function with returnValue
+
+function getInfo(){  
+return "hello javatpoint! How r u?";  
+}  
+document.write(getInfo());
